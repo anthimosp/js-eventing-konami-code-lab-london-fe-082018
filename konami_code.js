486a458a4c84a0b9452be436ad89130dbf667bae
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-08 18:51:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const input = document.querySelector('body')
 let index = 0
 
-function init() {
+function init(e) {
   const key = parseInt(e.detail || e.which);
   if (input === code[index]){index++;
     if (index === code.length){ alert("Congratulations!");
