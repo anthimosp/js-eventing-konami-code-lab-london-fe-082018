ceda36af35052140d8593b2610ff3f11974a8d99
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-08 18:45:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,13 +1,15 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 const input = document.querySelector('body')
+let index = 0
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  const key = parseInt(e.detail || e.which);
+  if (body === code[index])
 
 }
 
 input.addEventListener('keydown', e => {
   console.log(e)
-  
+  return init()
   }
 )
